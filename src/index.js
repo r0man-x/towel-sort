85966dbf440c853b0d228c33492fc4a97da5692d
r0man-x <snegovata@gmail.com>
2020-10-25 21:53:13 +0300

    Update index.js
    task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+    function arrSort( array, result, reverse = false) {
+        if (reverse) array.reverse();
+
+        array.forEach(element => {
+            result.push(element);
+        });
+    }
+
+    let arrResult = [];
+
+    if (matrix) {
+        matrix.forEach(function (element, index) {
+            if (index % 2 == 0) arrSort(element, arrResult);
+            else arrSort(element, arrResult, true);
+        });
+    }
+
+    return arrResult;
 }
